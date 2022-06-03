32a54d938a7eb3c304330289fb348cf0c9fcd3f9
sasalilime <salimtison@gmail.com>
2022-06-03 16:04:18 +0200

    pb removing

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
 "use strict";
-var itemsContainer = document.querySelectorAll('.items-container');
-console.log(typeof itemsContainer);
+const itemsContainer = document.querySelectorAll('.items-container');
+const addContainerListeners = (currentContainer) => {
+    const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn');
+    deleteBtnListeners(currentContainerDeletionBtn);
+};
+itemsContainer.forEach((container) => {
+    addContainerListeners(container);
+});
+const deleteBtnListeners = (btn) => {
+    btn.addEventListener('click', handleContainerDeletion);
+};
+const handleContainerDeletion = (event) => {
+    const btn = event.target;
+    const arrayBtn = [...document.querySelectorAll('.delete-container-btn')];
+    const containers = [...document.querySelectorAll('.items-container')];
+    containers[arrayBtn.indexOf(btn)].remove();
+};
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
 const itemsContainer = document.querySelectorAll('.items-container') as NodeListOf<HTMLDivElement>;
 
-console.log(typeof itemsContainer);
+const addContainerListeners = (currentContainer: HTMLDivElement) => {
+    const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement;
+    deleteBtnListeners(currentContainerDeletionBtn);
+};
+
+itemsContainer.forEach((container: HTMLDivElement) => {
+    addContainerListeners(container);
+});
+
+const deleteBtnListeners = (btn: HTMLButtonElement) => {
+    btn.addEventListener('click', handleContainerDeletion);
+};
+
+const handleContainerDeletion = (event: MouseEvent) => {
+    const btn = event.target as HTMLButtonElement;
+    const arrayBtn = [...document.querySelectorAll('.delete-container-btn')] as HTMLButtonElement[];
+    const containers = [...document.querySelectorAll('.items-container')] as HTMLDivElement[];
+    containers[arrayBtn.indexOf(btn)].remove();
+};
